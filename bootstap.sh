000cefadfaba181dede63ced0a008a9858c3e366
Alex Allen <alexander.ryan.allen@gmail.com>
2018-12-13 22:11:54 -0800

    attempting to automate some stuff

--- bootstap.sh
+++ bootstap.sh
@@ -4,3 +4,8 @@ ln -sv .zsrhc "$HOME"
 
 # Vim pluin install
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
+
---
+++ brewstrap.sh
@@ -0,0 +1,6 @@
+# zsh
+brew install zsh
+
+# fonts
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
